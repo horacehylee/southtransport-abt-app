99d5a1cc965230a5fb6574cdf1c3cb16b486be7f
Horace <horacehylee2@gmail.com>
2017-09-16 11:53:44 +0800

    added transport department news screen

--- src/screens.js
+++ src/screens.js
@@ -1,6 +1,8 @@
 import { Navigation } from "react-native-navigation"
 import { Main } from "./modules/main/Main"
+import TransportDepartmentNews from "./screens/TransportDepartmentNews"
 
 export function registerScreens(store, provider) {
     Navigation.registerComponent("abt.main", () => Main)
+    Navigation.registerComponent("abt.transportDepartmentNews", () => TransportDepartmentNews)
 }
---
+++ src/screens/TransportDepartmentNews.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { 
+    View,
+    Text,
+    StyleSheet,
+ } from 'react-native';
+
+ class TransportDepartmentNews extends Component {
+     state = {  }
+     render() {
+         return (
+             <View>
+                 <Text>Hello</Text>
+             </View>
+         );
+     }
+ }
+ 
+ export default TransportDepartmentNews;
