3f47072b616a603a3728c23fd156270a81a8117c
Horace <horacehylee2@gmail.com>
2017-09-16 18:43:41 +0800

    added splash screen

--- android/app/src/main/java/com/southtransport/southtransport_abt_app/MainActivity.java
+++ android/app/src/main/java/com/southtransport/southtransport_abt_app/MainActivity.java
@@ -3,4 +3,8 @@ package com.southtransport.southtransport_abt_app;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+    @Override
+    public int getSplashLayout() {
+        return R.layout.splash;
+    }
 }
